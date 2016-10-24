7328e6c1e0c008fab36d82350bbcb02305a09fb0
jmontara <jmontara@earthlink.net>
2016-10-24 08:14:56 -0400

    added logic and print lines to readme.py

--- readme.py
+++ readme.py
@@ -1,1 +1,10 @@
-print("readme.py")
+print("file name: readme.py")
+
+a = True
+
+if a == True:
+	print("if statement returns correct result")
+else:
+	print("if statement returns incorrect result")
+	
+print("readme test complete")
